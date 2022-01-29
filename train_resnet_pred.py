4a5282d1af5f7ef6001db9eb009d6420b113eef9
jijohn01 <jijohn01@naver.com>
2022-01-29 22:30:59 +0900

    [DEV] Add dropout & transformRotation

--- train_resnet_pred.py
+++ train_resnet_pred.py
@@ -53,6 +53,8 @@ def train(args):
                                            transforms.RandomCrop(224),
                                            transforms.RandomHorizontalFlip(p=0.5),
                                            transforms.RandomVerticalFlip(p=0.5),
+                                           transforms.RandomRotation(degrees=(0, 360)),
+                                           transforms.RandomPerspective(),
                                            transforms.ToTensor(),
                                            transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                 std=[0.229, 0.224, 0.225]),
